b25491aa9ed9ef434413d264e36adac7a7bdd43c
dingdangmaoup <dingdangmaoup@gmail.com>
2024-11-08 13:27:11 +0800

    初始化项目结构，添加 Vite、React 和 Tauri 支持，配置 Tailwind CSS 和 ESLint

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// prettier.config.js
+/**
+ * @type {import('prettier').Config}
+ * @see https://www.prettier.cn/docs/options.html
+ */
+export default {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
