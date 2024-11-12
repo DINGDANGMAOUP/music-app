013844c494a8e837e576cec03f63a6e84dcef025
dingdangmaoup <dingdangmaoup@gmail.com>
2024-11-12 10:24:01 +0800

    添加音乐播放器状态管理，集成音乐列表，优化播放控制逻辑，支持上一曲和下一曲功能

---
+++ src/atoms/musci-player-atom.ts
@@ -0,0 +1,24 @@
+export type Music = {
+  id: number;
+  title: string;
+  artist: string;
+  cover: string;
+  src: string;
+};
+export const musicListAtom = atom<Music[]>([
+  {
+    id: 1,
+    title: '50-tobylane',
+    artist: 'Jubel, NEIMY',
+    cover: 'https://i.scdn.co/image/ab67616d0000b273e2b5d1a5f4a3c9e3e1b6d3f5',
+    src: 'http://10.10.201.24:7100/music/50-tobylane.mp3',
+  },
+  {
+    id: 2,
+    title: 'cali-wataboi',
+    artist: 'Jubel, NEIMY',
+    cover: 'https://i.scdn.co/image/ab67616d0000b273e2b5d1a5f4a3c9e3e1b6d3f5',
+    src: 'http://10.10.201.24:7100/music/cali-wataboi.mp3',
+  },
+]);
+export const currentMusicIndexAtom = atom(0);
--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -1,11 +1,44 @@
-import mc from '@/assets/50-tobylane.mp3';
+import {
+  currentMusicIndexAtom,
+  musicListAtom,
+} from '@/atoms/musci-player-atom';
 import { List } from 'lucide-react';
+
 const MusicPlayer = () => {
   const audio = useRef<HTMLAudioElement | null>();
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   const [bufferProgress, setBufferProgress] = useState(0);
+  const musicList = useAtomValue(musicListAtom);
+  const [currentMusicIndex, setCurrentMusicIndex] = useAtom(
+    currentMusicIndexAtom,
+  );
+  const currentMusic = musicList?.[currentMusicIndex];
+  const { title, src } = currentMusic;
+
+  const reset = useCallback(() => {
+    //进度重置
+    setProgress(0);
+    //缓冲进度重置
+    setBufferProgress(0);
+    //播放进度重置
+    if (audio.current) audio.current.currentTime = 0;
+  }, []);
+  const handlePrevClick = useCallback(() => {
+    setCurrentMusicIndex(
+      (prev) => (prev - 1 + musicList.length) % musicList.length,
+    );
+    reset();
+  }, [musicList.length, reset, setCurrentMusicIndex]);
+  const handleNextClick = useCallback(() => {
+    setCurrentMusicIndex((prev) => (prev + 1) % musicList.length);
+    reset();
+  }, [musicList.length, reset, setCurrentMusicIndex]);
+  const handlePlayPauseClick = useCallback(() => {
+    setIsPlaying((prev) => !prev);
+  }, []);
+
   //获取音频缓冲进度
   const bufferUpdateListener = useCallback(() => {
     if (audio.current) {
@@ -23,38 +56,46 @@ const MusicPlayer = () => {
     return () => {
       audio.current?.removeEventListener('progress', bufferUpdateListener);
     };
-  }, [audio, bufferUpdateListener]);
+  }, [bufferUpdateListener]);
   const timeUpdateListener = useCallback(() => {
     setProgress(audio.current?.currentTime || 0);
   }, [audio]);
 
   useEffect(() => {
-    audio.current = new Audio(mc);
+    audio.current = new Audio(src);
     audio.current.addEventListener('timeupdate', timeUpdateListener);
     return () => {
       audio.current?.removeEventListener('timeupdate', timeUpdateListener);
     };
-  }, [audio, timeUpdateListener]);
+  }, [audio, src, timeUpdateListener]);
   useEffect(() => {
     if (isPlaying) {
       audio.current?.play();
-      // startTimer();
     } else {
       audio.current?.pause();
     }
   }, [isPlaying]);
-  const handlePlayPauseClick = useCallback(() => {
-    setIsPlaying((prev) => !prev);
-  }, []);
+  //自动播放下一首 循环
+  useEffect(() => {
+    const handleEnded = () => {
+      handleNextClick();
+    };
+    audio.current?.addEventListener('ended', handleEnded);
+    return () => {
+      audio.current?.removeEventListener('ended', handleEnded);
+    };
+  }, [audio, handleNextClick]);
+
   return (
     <div className="fixed bottom-0 left-[var(--sidebar-width)] right-0 flex h-12 flex-row items-center justify-between border-t bg-white/30 p-1 backdrop-blur-xl">
       <PlayerControls
         isPlaying={isPlaying}
         onPlayPauseClick={handlePlayPauseClick}
-        onPrevClick={() => {}}
-        onNextClick={() => {}}
+        onPrevClick={handlePrevClick}
+        onNextClick={handleNextClick}
       />
       <div className="mx-2 w-[200px] flex-grow">
+        title:{title},cache:{bufferProgress}
         <Slider
           value={[progress]}
           max={audio.current?.duration || 0}
